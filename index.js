ed81ae9e309c8f78221d1d611ffaad2fe077e7fa
Razym-codes <107475907+Razym-codes@users.noreply.github.com>
2022-07-19 21:31:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let randomNumber1 = Math.ceil(Math.random()*6);
+let randomNumber2 = Math.ceil(Math.random()*6);
+
+
+
+
+let x = document.querySelector("img").setAttribute("src", "images/dice" + randomNumber1 +".png");
+let y = document.querySelector(".dice2").setAttribute("src", "images/dice" + randomNumber2 +".png");
+
+if (randomNumber1 == randomNumber2) {
+  result = "Draw";
+}
+else{
+  result = randomNumber1 > randomNumber2 ? "Player 1 is the winner" : "Player 2 is the winner";
+}
+document.querySelector(".result").innerText = result;
